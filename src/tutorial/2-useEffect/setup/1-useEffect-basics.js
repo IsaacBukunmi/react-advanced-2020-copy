12b5feef755233ae53519613975341ed095a6eb3
Isaac <adetonaisaac12345@gmail.com>
2020-11-22 20:59:30 +0100

    useEffect Basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
-// second parameter
+// second parameter - Having the second parameter (dependency list - empty array []) makes the useEffect hook only runs on the initial render *** To rectify this and make it run multiple times, add value to the array.
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0)
+
+  // useEffect is used when we want to set up side effect which is some work outside the component which runs after every render
+  
+  useEffect(() => {
+    console.log('call useEffect')
+    // hooks cannot be called conditionally(i.e hooks in a conditional) but conditionals can be set in a hooks
+    if(value >= 1){
+      document.title = `New Message(${value})`
+    }
+  }, [value])
+
+  console.log('render component')
+  return(
+    <>
+      <h3>{value}</h3>
+      <button className="btn" onClick={() => setValue(value+1)}>Click me</button>
+    </>
+  )
 };
 
 export default UseEffectBasics;
