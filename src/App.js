171284e721c4afdd88f710b5e5136924f075f5d7
Isaac <adetonaisaac12345@gmail.com>
2020-12-01 22:39:35 +0100

    useReducer complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import Setup from './tutorial/5-useRef/setup/1-useRef-basics'
+import Setup from './tutorial/6-useReducer/setup'
 
 
 function App() {
+
   return (
     <div className='container'>
       <Setup />
